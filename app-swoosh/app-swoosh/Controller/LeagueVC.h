d6060c919071509e981b8a7cab3fd393b8ea39a9
Vladimir Milushev <vladimirmilushev@Vladimirs-MacBook-Air.local>
2017-10-08 16:19:39 +0300

    Refactoring

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.h
@@ -0,0 +1,13 @@
+//
+//  LeagueVC.h
+//  app-swoosh
+//
+//  Created by Vladimir Milushev on 8.10.17.
+//  Copyright © 2017 Vladimir Milushev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeagueVC : UIViewController
+
+@end
--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     @IBOutlet weak var mainLogo: UIImageView!
     @IBOutlet weak var backgroundImage: UIImageView!
